53cf507d70347e1fa8e1c9db0836d5841a7f952c
viols-code <viola.renne@gmail.com>
2022-03-08 18:29:00 +0100

    Player and AssistantCard

---
+++ src/main/java/it/polimi/ingsw/AssistantClass.java
@@ -0,0 +1,43 @@
+package it.polimi.ingsw;
+
+/**
+ * AssistantCard
+ *
+ * @author violarenne
+ * @version 1.0
+ */
+public class AssistantCard {
+    private final int value;
+    private final int motherNatureMovement;
+
+    /**
+     * Constructor: creates a new AssistantCard with the given value and motherNatureMovement.
+     *
+     * @param value value used for turn order
+     * @param motherNatureMovement movements Mother Nature may perform
+     */
+    public AssistantCard(int value, int motherNatureMovement) {
+        this.value = value;
+        this.motherNatureMovement = motherNatureMovement;
+    }
+
+    /**
+     * Get the card value
+     *
+     * @return value used for turn order
+     */
+    public int getValue() {
+        return value;
+    }
+
+    /**
+     * Get the motherNatureMovement
+     *
+     * @return movements Mother Nature may perform
+     */
+    public int getMotherNatureMovement() {
+        return motherNatureMovement;
+    }
+
+
+}
