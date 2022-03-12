219168b5f3a0696a995cb671da58be4a88018773
viols-code <viola.renne@gmail.com>
2022-03-12 12:19:00 +0100

    GamePhase class added

---
+++ src/main/java/it/polimi/ingsw/GamePhase.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw;
+
+/**
+ * Enumeration of the phases in the game
+ *
+ * @version 1.0
+ */
+public enum GamePhase {
+    SET_CLOUD_TILE,
+    PLAY_ASSISTANT_CARD,
+    MOVE_STUDENT,
+    MOVE_MOTHER_NATURE,
+    CHOOSE_CLOUD_TILE,
+}
