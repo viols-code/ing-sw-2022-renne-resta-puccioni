f1481f3168eaafd46fee90e07ee657e3d658d3f7
sararesta <sara.resta00@gmail.com>
2022-03-13 15:04:15 +0100

    Added test for addStudentToDiningRoom in PlayerTest

--- src/test/java/it/polimi/ingsw/PlayerTest.java
+++ src/test/java/it/polimi/ingsw/PlayerTest.java
@@ -18,8 +18,28 @@ class PlayerTest {
 
     @Test
     void addStudentToDiningRoom() {
-        player1.addStudentToDiningRoom(Colour.PINK);
-        assertEquals(1, player1.getDiningRoom(Colour.PINK));
+        int nPink,nYellow,nBlue,nGreen,nRed;
+        nPink=4;
+        nBlue=3;
+        nGreen=2;
+        nYellow=1;
+        nRed=0;
+        for(int i=0;i<nPink;i++)
+            player1.addStudentToDiningRoom(Colour.PINK);
+        for(int i=0;i<nBlue;i++)
+            player1.addStudentToDiningRoom(Colour.BLUE);
+        for(int i=0;i<nGreen;i++)
+            player1.addStudentToDiningRoom(Colour.GREEN);
+        for(int i=0;i<nYellow;i++)
+            player1.addStudentToDiningRoom(Colour.YELLOW);
+        for(int i=0;i<nRed;i++)
+            player1.addStudentToDiningRoom(Colour.RED);
+
+        assertEquals(nPink, player1.getDiningRoom(Colour.PINK));
+        assertEquals(nBlue, player1.getDiningRoom(Colour.BLUE));
+        assertEquals(nGreen, player1.getDiningRoom(Colour.GREEN));
+        assertEquals(nYellow, player1.getDiningRoom(Colour.YELLOW));
+        assertEquals(nRed, player1.getDiningRoom(Colour.RED));
     }
 
     @Test
