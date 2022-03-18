d8ac6c0f01545b0d2d60c49c7f57f4ba2b6651c0
viols-code <viola.renne@gmail.com>
2022-03-19 00:04:56 +0100

    Added the javadoc

--- src/main/java/it/polimi/ingsw/model/card/BasicState.java
+++ src/main/java/it/polimi/ingsw/model/card/BasicState.java
@@ -3,8 +3,12 @@ package it.polimi.ingsw.model.card;
 import it.polimi.ingsw.model.game.Game;
 
 public class BasicState extends CharacterCard {
+    /**
+     * Constructor: creates the BasicState
+     *
+     * @param game the Game
+     */
     public BasicState(Game game) {
         super(game);
     }
-
 }
