672fa3014d5e618df7571ded16cff214cb3c741b
viols-code <viola.renne@gmail.com>
2022-03-19 17:15:00 +0100

    Created GameControllerTest

---
+++ src/test/java/it/polimi/ingsw/controller/GameControllerTest.java
@@ -0,0 +1,26 @@
+package it.polimi.ingsw.controller;
+
+import it.polimi.ingsw.model.game.ExpertGame;
+import it.polimi.ingsw.model.game.Game;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+    @Test
+    void playCharacterCard() {
+    }
+
+    @Test
+    void playAssistantCard() {
+    }
+
+    @Test
+    void addPlayer() {
+    }
+}
