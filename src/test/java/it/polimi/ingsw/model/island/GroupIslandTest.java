3f4f4bdeed0915609e2d116244b1907f93514ffe
viols-code <viola.renne@gmail.com>
2022-03-19 10:00:06 +0100

    Created a new GroupIslandTest

---
+++ src/test/java/it/polimi/ingsw/model/island/GroupIslandTest.java
@@ -0,0 +1,45 @@
+package it.polimi.ingsw.model.island;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GroupIslandTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void addSingleIsland() {
+    }
+
+    @Test
+    void removeMotherNature() {
+    }
+
+    @Test
+    void placeMotherNature() {
+    }
+
+    @Test
+    void changeInfluence() {
+    }
+
+    @Test
+    void isNoEntryTile() {
+    }
+
+    @Test
+    void getNumberOfNoEntryTile() {
+    }
+
+    @Test
+    void addNoEntryTile() {
+    }
+
+    @Test
+    void removeNoEntryTile() {
+    }
+}
