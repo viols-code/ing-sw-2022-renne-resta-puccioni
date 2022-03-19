cd5a293427f877c758ca0a65b4398756d3ba352c
viols-code <viola.renne@gmail.com>
2022-03-19 17:40:07 +0100

    Added getCoin test in PlayerTest

--- src/test/java/it/polimi/ingsw/model/player/PlayerTest.java
+++ src/test/java/it/polimi/ingsw/model/player/PlayerTest.java
@@ -3,60 +3,52 @@ package it.polimi.ingsw.model.player;
 import it.polimi.ingsw.model.AssistantCard;
 import it.polimi.ingsw.model.game.BasicGame;
 import it.polimi.ingsw.model.game.Game;
-import it.polimi.ingsw.model.player.Wizard;
-import it.polimi.ingsw.model.player.BasicPlayer;
-import it.polimi.ingsw.model.player.Player;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
-    private Player player1;
+    private Player playerTest;
     private Game game;
 
     @BeforeEach
     void setUp() {
-        player1 = new BasicPlayer("Sara", Wizard.TYPE_2);
+        playerTest = new BasicPlayer("Sara", Wizard.TYPE_2);
         game = new BasicGame();
     }
 
     @Test
-    void isAssistantCardPresent(){
-
-    }
-
-    @Test
     void addAssistantCard(){
-        AssistantCard card= game.getAssistantCard(1);
-        player1.addAssistantCard(card);
-        assertTrue(player1.isAssistantCardPresent(card));
+        AssistantCard card = game.getAssistantCard(1);
+        playerTest.addAssistantCard(card);
+        assertTrue(playerTest.isAssistantCardPresent(card));
     }
 
     @Test
     void removeAssistantCard() {
         AssistantCard card= game.getAssistantCard(1);
-        player1.addAssistantCard(card);
-        player1.removeAssistantCard(card);
-        assertFalse(player1.isAssistantCardPresent(card));
+        playerTest.addAssistantCard(card);
+        playerTest.removeAssistantCard(card);
+        assertFalse(playerTest.isAssistantCardPresent(card));
     }
 
     @Test
     void setCurrentAssistantCard() {
         AssistantCard card= game.getAssistantCard(1);
-        player1.addAssistantCard(card);
-        player1.setCurrentAssistantCard(card);
-        assertEquals(card,player1.getCurrentAssistantCard());
+        playerTest.addAssistantCard(card);
+        playerTest.setCurrentAssistantCard(card);
+        assertEquals(card,playerTest.getCurrentAssistantCard());
     }
 
+    @Test
+    void getCoin(){ assertThrows(IllegalAccessError.class, () -> playerTest.getCoins()); }
 
     @Test
     void addCoins() {
-        assertThrows(IllegalAccessError.class, () -> { player1.addCoins(10); } );
-    }
+        assertThrows(IllegalAccessError.class, () ->  playerTest.addCoins(10)); }
 
     @Test
     void removeCoins() {
-        assertThrows(IllegalAccessError.class, () -> { player1.removeCoins(10); } );
-    }
+        assertThrows(IllegalAccessError.class, () -> playerTest.removeCoins(10)); }
 }
