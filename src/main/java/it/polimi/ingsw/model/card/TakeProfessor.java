5da9f250f7a2d13035243a40df961ab0931c43ee
Laura Puccioni <laura.puccioni@mail.polimi.it>
2022-03-20 15:31:58 +0100

    takeProfessor

--- src/main/java/it/polimi/ingsw/model/card/TakeProfessor.java
+++ src/main/java/it/polimi/ingsw/model/card/TakeProfessor.java
@@ -1,9 +1,29 @@
 package it.polimi.ingsw.model.card;
 
+import it.polimi.ingsw.model.Colour;
 import it.polimi.ingsw.model.game.Game;
 
 public class TakeProfessor extends CharacterCard {
+    /**
+     * Constructor
+     *
+     * @param game
+     */
     public TakeProfessor(Game game) {
         super(game);
+    }
+
+    /**
+     * Take the professor of the colour given if possible
+     *
+     * @param colour
+     */
+    @Override
+    public void checkProfessor(Colour colour){
+        for(int i = 0; i<game.getNumberOfPlayer(); i++){
+                if (game.getCurrentPlayer().getSchoolBoard().getDiningRoom(colour) >= game.getPlayerByIndex(i).getSchoolBoard().getDiningRoom(colour)) {
+                    game.getCurrentPlayer().getSchoolBoard().hasProfessor(colour);
+                }
+        }
     }
 }
