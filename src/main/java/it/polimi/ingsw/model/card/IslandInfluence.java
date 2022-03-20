e20a753639aea5a83b9ef66c59c6be0897abbb1c
Laura Puccioni <laura.puccioni@mail.polimi.it>
2022-03-20 15:38:43 +0100

    IslandInfluence

--- src/main/java/it/polimi/ingsw/model/card/IslandInfluence.java
+++ src/main/java/it/polimi/ingsw/model/card/IslandInfluence.java
@@ -1,9 +1,41 @@
 package it.polimi.ingsw.model.card;
 
 import it.polimi.ingsw.model.game.Game;
+import it.polimi.ingsw.model.table.island.GroupIsland;
 
 public class IslandInfluence extends CharacterCard {
+
+    /**
+     * The groupIsland chosen
+     */
+    GroupIsland islandChosen;
+
+    /**
+     * Constructor
+     *
+     * @param game
+     */
     public IslandInfluence(Game game) {
         super(game);
+    }
+
+    /**
+     * Set the island to choose
+     *
+     * @param groupIsland
+     */
+    @Override
+    public void setIsland(GroupIsland groupIsland){
+        this.islandChosen = groupIsland;
+        this.effect();
+    }
+
+    /**
+     * Activates the effect of the characterCard
+     */
+    @Override
+    public void effect(){
+
+        calculateInfluence(game.getCurrentPlayer(), islandChosen);
     }
 }
