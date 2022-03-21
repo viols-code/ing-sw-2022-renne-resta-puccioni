4839f1cd259578643f669f154f21bbc9ba80520a
Laura Puccioni <laura.puccioni@mail.polimi.it>
2022-03-21 20:30:08 +0100

    MotherNatureMovement

--- src/main/java/it/polimi/ingsw/model/card/MotherNatureMovement.java
+++ src/main/java/it/polimi/ingsw/model/card/MotherNatureMovement.java
@@ -3,7 +3,18 @@ package it.polimi.ingsw.model.card;
 import it.polimi.ingsw.model.game.Game;
 
 public class MotherNatureMovement extends CharacterCard {
+
+    int possibleMovement;
+
     public MotherNatureMovement(Game game) {
         super(game);
+    }
+
+    public void effect(){
+         possibleMovement = game.getCurrentPlayer().getCurrentAssistantCard().getMotherNatureMovement() + 2;
+
+         /*
+         to do
+          */
     }
 }
