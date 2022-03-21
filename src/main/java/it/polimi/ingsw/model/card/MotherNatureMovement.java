b92b4b90ae8f244d5eda6b3872777c632108e3a7
Laura Puccioni <laura.puccioni@mail.polimi.it>
2022-03-21 21:48:00 +0100

    MotherNatureMovement

--- src/main/java/it/polimi/ingsw/model/card/MotherNatureMovement.java
+++ src/main/java/it/polimi/ingsw/model/card/MotherNatureMovement.java
@@ -1,20 +1,38 @@
 package it.polimi.ingsw.model.card;
 
+import it.polimi.ingsw.model.AssistantCard;
 import it.polimi.ingsw.model.game.Game;
 
 public class MotherNatureMovement extends CharacterCard {
 
+    /**
+     * New movement allowed for motherNatured
+     */
     int possibleMovement;
 
+    /**
+     * New assistantCard with the updated motherNatureMovement
+     */
+    AssistantCard newCard;
+
+    /**
+     * Contructor
+     * @param game
+     */
     public MotherNatureMovement(Game game) {
         super(game);
     }
 
+    /**
+     * Activates the effect of the CharacterCard
+     */
     public void effect(){
-         possibleMovement = game.getCurrentPlayer().getCurrentAssistantCard().getMotherNatureMovement() + 2;
 
-         /*
-         to do
-          */
+        possibleMovement = game.getCurrentPlayer().getCurrentAssistantCard().getMotherNatureMovement() + 2;
+        newCard = new AssistantCard(game.getCurrentPlayer().getCurrentAssistantCard().getValue(), possibleMovement);
+        game.getCurrentPlayer().setCurrentAssistantCard(newCard);
+
+        game.setActiveCharacterCard(game.getBasicState());
+
     }
 }
