95863096057a349c1c13f18e591e5f20ba92455b
sararesta <sara.resta00@gmail.com>
2022-03-24 20:43:56 +0100

    Created CharacterCardTest

---
+++ src/test/java/it/polimi/ingsw/model/card/CharacterCardTest.java
@@ -0,0 +1,77 @@
+package it.polimi.ingsw.model.card;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CharacterCardTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void setting() {
+    }
+
+    @Test
+    void getCost() {
+    }
+
+    @Test
+    void incrementCost() {
+    }
+
+    @Test
+    void effect() {
+    }
+
+    @Test
+    void calculateInfluencePlayer() {
+    }
+
+    @Test
+    void calculateInfluence() {
+    }
+
+    @Test
+    void changeInfluenceGroupIsland() {
+    }
+
+    @Test
+    void checkProfessor() {
+    }
+
+    @Test
+    void professor() {
+    }
+
+    @Test
+    void checkMotherNatureMovement() {
+    }
+
+    @Test
+    void setColour() {
+    }
+
+    @Test
+    void setColourAndIsland() {
+    }
+
+    @Test
+    void setGroupIsland() {
+    }
+
+    @Test
+    void setColourDiningRoomEntrance() {
+    }
+
+    @Test
+    void setColourCardEntrance() {
+    }
+
+    @Test
+    void checkUnifyIsland() {
+    }
+}
