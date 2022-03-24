35be963e2624ae85cfc469cf4a295204c7a5632d
viols-code <viola.renne@gmail.com>
2022-03-24 12:04:31 +0100

    Solved problems

--- src/test/java/it/polimi/ingsw/model/table/island/GroupIslandTest.java
+++ src/test/java/it/polimi/ingsw/model/table/island/GroupIslandTest.java
@@ -25,77 +25,94 @@ class GroupIslandTest {
         int numberGreen = 0;
         int numberPink = 0;
         int numberYellow = 0;
+        int num;
 
         for(int i = 0; i < gameController.getGame().getTable().getNumberOfGroupIsland(); i++){
+            num = gameController.getGame().getTable().getGroupIslandByIndex(i).getIslandByIndex(0).getStudents(Colour.BLUE);
+            numberBlue += num;
+            num = gameController.getGame().getTable().getGroupIslandByIndex(i).getIslandByIndex(0).getStudents(Colour.RED);
+            numberRed += num;
+            num = gameController.getGame().getTable().getGroupIslandByIndex(i).getIslandByIndex(0).getStudents(Colour.GREEN);
+            numberGreen+= num;
+            num = gameController.getGame().getTable().getGroupIslandByIndex(i).getIslandByIndex(0).getStudents(Colour.PINK);
+            numberPink+= num;
+            num = gameController.getGame().getTable().getGroupIslandByIndex(i).getIslandByIndex(0).getStudents(Colour.YELLOW);
+            numberYellow+= num;
 
         }
+
+        assertEquals(2, numberBlue);
+        assertEquals(2, numberRed);
+        assertEquals(2, numberGreen);
+        assertEquals(2, numberPink);
+        assertEquals(2, numberYellow);
     }
 
     @Test
     void addSingleIsland() {
-        assertEquals(12, game.getTable().getNumberOfGroupIsland());
-        int number1 = game.getTable().getGroupIslandByIndex(0).getNumberStudent(Colour.BLUE);
-        int number2 = game.getTable().getGroupIslandByIndex(1).getNumberStudent(Colour.BLUE);
+        assertEquals(12, gameController.getGame().getTable().getNumberOfGroupIsland());
+        int number1 = gameController.getGame().getTable().getGroupIslandByIndex(0).getNumberStudent(Colour.BLUE);
+        int number2 = gameController.getGame().getTable().getGroupIslandByIndex(1).getNumberStudent(Colour.BLUE);
 
-        for (int i = 0; i < game.getTable().getNumberOfGroupIsland(); i++) {
-            assertEquals(1, game.getTable().getGroupIslandByIndex(i).getNumberOfSingleIsland());
+        for (int i = 0; i < gameController.getGame().getTable().getNumberOfGroupIsland(); i++) {
+            assertEquals(1, gameController.getGame().getTable().getGroupIslandByIndex(i).getNumberOfSingleIsland());
         }
 
-        game.getTable().getGroupIslandByIndex(0).addSingleIsland(game.getTable().getGroupIslandByIndex(1).getIslandByIndex(0));
-        game.getTable().removeGroupIsland(game.getTable().getGroupIslandByIndex(1));
+        gameController.getGame().getTable().getGroupIslandByIndex(0).addSingleIsland(gameController.getGame().getTable().getGroupIslandByIndex(1).getIslandByIndex(0));
+        gameController.getGame().getTable().removeGroupIsland(gameController.getGame().getTable().getGroupIslandByIndex(1));
 
-        assertEquals(2, game.getTable().getGroupIslandByIndex(0).getNumberOfSingleIsland());
-        assertEquals(11, game.getTable().getNumberOfGroupIsland());
-        assertEquals(number1 + number2, game.getTable().getGroupIslandByIndex(0).getNumberStudent(Colour.BLUE));
+        assertEquals(2, gameController.getGame().getTable().getGroupIslandByIndex(0).getNumberOfSingleIsland());
+        assertEquals(11, gameController.getGame().getTable().getNumberOfGroupIsland());
+        assertEquals(number1 + number2, gameController.getGame().getTable().getGroupIslandByIndex(0).getNumberStudent(Colour.BLUE));
     }
 
     @Test
     void removeMotherNature() {
-        game.getTable().setMotherNaturePosition(0);
-        assertTrue(game.getTable().getGroupIslandByIndex(0).getMotherNature());
-        game.getTable().getGroupIslandByIndex(0).removeMotherNature();
-        game.getTable().getGroupIslandByIndex(1).placeMotherNature();
-        assertFalse(game.getTable().getGroupIslandByIndex(0).getMotherNature());
-        assertTrue(game.getTable().getGroupIslandByIndex(1).getMotherNature());
+        gameController.getGame().getTable().setMotherNaturePosition(0);
+        assertTrue(gameController.getGame().getTable().getGroupIslandByIndex(0).getMotherNature());
+        gameController.getGame().getTable().getGroupIslandByIndex(0).removeMotherNature();
+        gameController.getGame().getTable().getGroupIslandByIndex(1).placeMotherNature();
+        assertFalse(gameController.getGame().getTable().getGroupIslandByIndex(0).getMotherNature());
+        assertTrue(gameController.getGame().getTable().getGroupIslandByIndex(1).getMotherNature());
     }
 
     @Test
     void placeMotherNature() {
-        game.getTable().setMotherNaturePosition(0);
-        assertTrue(game.getTable().getGroupIslandByIndex(0).getMotherNature());
-        game.getTable().getGroupIslandByIndex(0).removeMotherNature();
-        game.getTable().getGroupIslandByIndex(2).placeMotherNature();
-        assertFalse(game.getTable().getGroupIslandByIndex(0).getMotherNature());
-        assertTrue(game.getTable().getGroupIslandByIndex(2).getMotherNature());
+        gameController.getGame().getTable().setMotherNaturePosition(0);
+        assertTrue(gameController.getGame().getTable().getGroupIslandByIndex(0).getMotherNature());
+        gameController.getGame().getTable().getGroupIslandByIndex(0).removeMotherNature();
+        gameController.getGame().getTable().getGroupIslandByIndex(2).placeMotherNature();
+        assertFalse(gameController.getGame().getTable().getGroupIslandByIndex(0).getMotherNature());
+        assertTrue(gameController.getGame().getTable().getGroupIslandByIndex(2).getMotherNature());
     }
 
     @Test
     void changeInfluence() {
         Player playerTest = new BasicPlayer("Viola", Wizard.TYPE_4);
-        assertNull(game.getTable().getGroupIslandByIndex(0).getInfluence());
-        game.getTable().getGroupIslandByIndex(0).changeInfluence(playerTest);
-        assertEquals(playerTest, game.getTable().getGroupIslandByIndex(0).getInfluence());
+        assertNull(gameController.getGame().getTable().getGroupIslandByIndex(0).getInfluence());
+        gameController.getGame().getTable().getGroupIslandByIndex(0).changeInfluence(playerTest);
+        assertEquals(playerTest, gameController.getGame().getTable().getGroupIslandByIndex(0).getInfluence());
     }
 
     @Test
     void isNoEntryTile() {
-        for (int i = 0; i < game.getTable().getNumberOfGroupIsland(); i++) {
-            assertFalse(game.getTable().getGroupIslandByIndex(i).isNoEntryTile());
+        for (int i = 0; i < gameController.getGame().getTable().getNumberOfGroupIsland(); i++) {
+            assertFalse(gameController.getGame().getTable().getGroupIslandByIndex(i).isNoEntryTile());
         }
     }
 
     @Test
     void getNumberOfNoEntryTile() {
-        assertThrows(IllegalAccessError.class, () -> game.getTable().getGroupIslandByIndex(0).getNumberOfNoEntryTile());
+        assertThrows(IllegalAccessError.class, () -> gameController.getGame().getTable().getGroupIslandByIndex(0).getNumberOfNoEntryTile());
     }
 
     @Test
     void addNoEntryTile() {
-        assertThrows(IllegalAccessError.class, () -> game.getTable().getGroupIslandByIndex(1).addNoEntryTile());
+        assertThrows(IllegalAccessError.class, () -> gameController.getGame().getTable().getGroupIslandByIndex(1).addNoEntryTile());
     }
 
     @Test
     void removeNoEntryTile() {
-        assertThrows(IllegalAccessError.class, () -> game.getTable().getGroupIslandByIndex(2).removeNoEntryTile());
+        assertThrows(IllegalAccessError.class, () -> gameController.getGame().getTable().getGroupIslandByIndex(2).removeNoEntryTile());
     }
 }
