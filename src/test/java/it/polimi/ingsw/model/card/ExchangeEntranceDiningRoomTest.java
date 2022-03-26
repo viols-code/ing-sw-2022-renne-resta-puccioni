4abf49b5517e9d66f2eaa455599d90ac1429b20b
sararesta <sara.resta00@gmail.com>
2022-03-26 12:02:21 +0100

    Added setUp to ExchangeEntranceDiningRoomTest

--- src/test/java/it/polimi/ingsw/model/card/ExchangeEntranceDiningRoomTest.java
+++ src/test/java/it/polimi/ingsw/model/card/ExchangeEntranceDiningRoomTest.java
@@ -1,14 +1,29 @@
 package it.polimi.ingsw.model.card;
 
+import it.polimi.ingsw.model.game.ExpertGame;
+import it.polimi.ingsw.model.game.Game;
+import it.polimi.ingsw.model.player.ExpertPlayer;
+import it.polimi.ingsw.model.player.Wizard;
+import it.polimi.ingsw.model.table.island.BasicGroupIsland;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ExchangeEntranceDiningRoomTest {
+    private Game gameTest;
+    private NoColour cardTest;
 
     @BeforeEach
     void setUp() {
+        gameTest = new ExpertGame();
+        cardTest = new NoColour(gameTest);
+        gameTest.addCharacterCard(cardTest);
+        gameTest.addPlayer(new ExpertPlayer("sara", Wizard.TYPE_2));
+        gameTest.addPlayer(new ExpertPlayer("sara",Wizard.TYPE_3));
+        for (int i = 0; i < 12; i++) {
+            gameTest.getTable().addGroupIsland(new BasicGroupIsland());
+        }
     }
 
     @Test
