a7f3f8557edf38f76d7dc6d67d62509d8146822e
sararesta <sara.resta00@gmail.com>
2022-03-26 11:08:05 +0100

    Added setUp in NoColourTest

--- src/test/java/it/polimi/ingsw/model/card/NoColourTest.java
+++ src/test/java/it/polimi/ingsw/model/card/NoColourTest.java
@@ -1,21 +1,35 @@
 package it.polimi.ingsw.model.card;
 
+import it.polimi.ingsw.controller.GameController;
+import it.polimi.ingsw.model.Colour;
+import it.polimi.ingsw.model.game.ExpertGame;
+import it.polimi.ingsw.model.game.Game;
+import it.polimi.ingsw.model.player.ExpertPlayer;
+import it.polimi.ingsw.model.player.Player;
+import it.polimi.ingsw.model.player.Wizard;
+import it.polimi.ingsw.model.table.island.BasicGroupIsland;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class NoColourTest {
-
+    private Game gameTest;
+    private NoColour cardTest;
     @BeforeEach
     void setUp() {
+        gameTest = new ExpertGame();
+        cardTest = new NoColour(gameTest);
+        gameTest.addCharacterCard(cardTest);
     }
 
     @Test
     void setColour() {
+        
     }
 
     @Test
     void calculateInfluencePlayer() {
     }
+
 }
