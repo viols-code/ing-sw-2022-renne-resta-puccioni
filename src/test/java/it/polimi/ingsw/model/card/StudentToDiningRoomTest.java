d89cd2909192576fbaa683c3151fa8de0a45ed68
sararesta <sara.resta00@gmail.com>
2022-03-26 14:36:47 +0100

    Added setUp to StudentToDiningRoomTest

--- src/test/java/it/polimi/ingsw/model/card/StudentToDiningRoomTest.java
+++ src/test/java/it/polimi/ingsw/model/card/StudentToDiningRoomTest.java
@@ -1,13 +1,42 @@
 package it.polimi.ingsw.model.card;
 
+import it.polimi.ingsw.model.Colour;
+import it.polimi.ingsw.model.game.ExpertGame;
+import it.polimi.ingsw.model.game.Game;
+import it.polimi.ingsw.model.player.ExpertPlayer;
+import it.polimi.ingsw.model.player.Wizard;
+import it.polimi.ingsw.model.table.island.BasicGroupIsland;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StudentToDiningRoomTest {
 
-    @Test
-    void setting() {
+    private Game gameTest;
+    private ExchangeEntranceDiningRoom cardTest;
+
+    @BeforeEach
+    void setUp() {
+        gameTest = new ExpertGame();
+        cardTest = new ExchangeEntranceDiningRoom(gameTest);
+        gameTest.addCharacterCard(cardTest);
+        gameTest.addPlayer(new ExpertPlayer("sara", Wizard.TYPE_2));
+        gameTest.addPlayer(new ExpertPlayer("viola",Wizard.TYPE_3));
+        for (int i = 0; i < 12; i++) {
+            gameTest.getTable().addGroupIsland(new BasicGroupIsland());
+        }
+        gameTest.setCurrentPlayer(gameTest.getPlayerByIndex(0));
+
+        gameTest.getPlayerByIndex(0).getSchoolBoard().addStudentToDiningRoom(Colour.YELLOW);
+        gameTest.getPlayerByIndex(0).getSchoolBoard().addStudentToDiningRoom(Colour.YELLOW);
+        gameTest.getPlayerByIndex(0).getSchoolBoard().addStudentToDiningRoom(Colour.YELLOW);
+        gameTest.getPlayerByIndex(1).getSchoolBoard().addStudentToDiningRoom(Colour.GREEN);
+        gameTest.getPlayerByIndex(0).getSchoolBoard().addStudentToEntrance(Colour.GREEN);
+        gameTest.getPlayerByIndex(0).getSchoolBoard().addStudentToEntrance(Colour.GREEN);
+
+        gameTest.getPlayerByIndex(0).getSchoolBoard().addProfessor(Colour.YELLOW);
+        gameTest.getPlayerByIndex(1).getSchoolBoard().addProfessor(Colour.GREEN);
     }
 
     @Test
