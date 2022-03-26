5b856fd4f93ffa8014637912026b09ef2248aad3
sararesta <sara.resta00@gmail.com>
2022-03-26 15:07:22 +0100

    Implemented test for StudentToDiningRoomTest

--- src/test/java/it/polimi/ingsw/model/card/StudentToDiningRoomTest.java
+++ src/test/java/it/polimi/ingsw/model/card/StudentToDiningRoomTest.java
@@ -9,34 +9,31 @@ import it.polimi.ingsw.model.table.island.BasicGroupIsland;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StudentToDiningRoomTest {
 
     private Game gameTest;
-    private ExchangeEntranceDiningRoom cardTest;
+    private StudentToDiningRoom cardTest;
 
     @BeforeEach
     void setUp() {
         gameTest = new ExpertGame();
-        cardTest = new ExchangeEntranceDiningRoom(gameTest);
+        cardTest = new StudentToDiningRoom(gameTest);
         gameTest.addCharacterCard(cardTest);
         gameTest.addPlayer(new ExpertPlayer("sara", Wizard.TYPE_2));
         gameTest.addPlayer(new ExpertPlayer("viola",Wizard.TYPE_3));
-        for (int i = 0; i < 12; i++) {
-            gameTest.getTable().addGroupIsland(new BasicGroupIsland());
-        }
         gameTest.setCurrentPlayer(gameTest.getPlayerByIndex(0));
-
-        gameTest.getPlayerByIndex(0).getSchoolBoard().addStudentToDiningRoom(Colour.YELLOW);
-        gameTest.getPlayerByIndex(0).getSchoolBoard().addStudentToDiningRoom(Colour.YELLOW);
-        gameTest.getPlayerByIndex(0).getSchoolBoard().addStudentToDiningRoom(Colour.YELLOW);
-        gameTest.getPlayerByIndex(1).getSchoolBoard().addStudentToDiningRoom(Colour.GREEN);
-        gameTest.getPlayerByIndex(0).getSchoolBoard().addStudentToEntrance(Colour.GREEN);
-        gameTest.getPlayerByIndex(0).getSchoolBoard().addStudentToEntrance(Colour.GREEN);
-
-        gameTest.getPlayerByIndex(0).getSchoolBoard().addProfessor(Colour.YELLOW);
-        gameTest.getPlayerByIndex(1).getSchoolBoard().addProfessor(Colour.GREEN);
+        settingBag();
+        cardTest.setting();
+        for(Colour colour: Colour.values()){
+            gameTest.getPlayerByIndex(0).getSchoolBoard().addStudentToDiningRoom(colour);
+            gameTest.getPlayerByIndex(1).getSchoolBoard().addStudentToDiningRoom(colour);
+            gameTest.getPlayerByIndex(1).getSchoolBoard().addProfessor(colour);
+        }
     }
 
     @Test
@@ -45,5 +42,24 @@ class StudentToDiningRoomTest {
 
     @Test
     void setColour() {
+        List<Colour> colourList=new ArrayList<>();
+        for(Colour colour: Colour.values()){
+            if(cardTest.getStudents(colour)>0){
+                colourList.add(colour);
+            }
+        }
+        cardTest.setColour(colourList.get(0));
+        assertEquals(2,gameTest.getPlayerByIndex(0).getSchoolBoard().getDiningRoom(colourList.get(0)));
+        assertTrue(gameTest.getPlayerByIndex(0).getSchoolBoard().hasProfessor(colourList.get(0)));
+        assertFalse(gameTest.getPlayerByIndex(1).getSchoolBoard().hasProfessor(colourList.get(0)));
+    }
+
+    private void settingBag() {
+
+        for (Colour colour : Colour.values()) {
+            for (int i = 0; i < 24; i++) {
+                gameTest.getTable().getBag().addStudentBag(colour);
+            }
+        }
     }
 }
