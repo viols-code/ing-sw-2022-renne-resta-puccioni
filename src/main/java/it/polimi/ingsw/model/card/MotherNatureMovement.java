d9a98cf5dac9bd725fb9129fd0d39e02d24a16dd
Laura Puccioni <laura.puccioni@mail.polimi.it>
2022-03-27 15:21:40 +0200

    MotherNatureMovement

--- src/main/java/it/polimi/ingsw/model/card/MotherNatureMovement.java
+++ src/main/java/it/polimi/ingsw/model/card/MotherNatureMovement.java
@@ -1,19 +1,7 @@
 package it.polimi.ingsw.model.card;
-
-import it.polimi.ingsw.model.AssistantCard;
 import it.polimi.ingsw.model.game.Game;
 
 public class MotherNatureMovement extends CharacterCard {
-
-    /**
-     * New movement allowed for motherNatured
-     */
-    int possibleMovement;
-
-    /**
-     * New assistantCard with the updated motherNatureMovement
-     */
-    AssistantCard newCard;
 
     /**
      * Constructor
