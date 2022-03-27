5a1d65c2c103147064ca6c2e478a062e0c86fd6c
viols-code <viola.renne@gmail.com>
2022-03-27 14:24:31 +0200

    Reformat code

--- src/main/java/it/polimi/ingsw/model/player/Player.java
+++ src/main/java/it/polimi/ingsw/model/player/Player.java
@@ -64,7 +64,7 @@ public abstract class Player {
      *
      * @return a string with the nickname
      */
-    public synchronized String getNickname() {
+    public String getNickname() {
         return nickname;
     }
 
