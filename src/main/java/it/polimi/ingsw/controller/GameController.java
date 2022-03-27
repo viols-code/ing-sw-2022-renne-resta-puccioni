0301d3986598c978a2c3031cb10a9cbafd79f0e1
sararesta <sara.resta00@gmail.com>
2022-03-27 17:43:16 +0200

    solved problems

--- src/main/java/it/polimi/ingsw/controller/GameController.java
+++ src/main/java/it/polimi/ingsw/controller/GameController.java
@@ -306,8 +306,8 @@ public class GameController {
                     if (game.getPlayerByIndex(player).getSchoolBoard().getEntrance(colour) > 0) {
                         if (checkStudentsMovement(player) && game.getPlayerByIndex(player).getSchoolBoard().getDiningRoom(colour) < 10) {
                             game.getPlayerByIndex(player).getSchoolBoard().removeStudentFromEntrance(colour);
-                            game.getPlayerByIndex(player).getSchoolBoard().addStudentToDiningRoom(colour);
                             try {
+                                game.getPlayerByIndex(player).getSchoolBoard().addStudentToDiningRoom(colour);
                                 if (isGameExpert && ((game.getPlayerByIndex(player).getSchoolBoard().getDiningRoom(colour) + 1) % 3) == 0 && game.getCoins() > 0) {
                                     game.getPlayerByIndex(player).addCoins(1);
                                     game.setCoins(game.getCoins() - 1);
