7226145471e05e9d60051c418437aee25be08b8f
viols-code <viola.renne@gmail.com>
2022-03-27 11:03:47 +0200

    Added a test in ExpertGameTest

--- src/test/java/it/polimi/ingsw/model/game/ExpertGameTest.java
+++ src/test/java/it/polimi/ingsw/model/game/ExpertGameTest.java
@@ -4,7 +4,9 @@ import it.polimi.ingsw.controller.GameController;
 import it.polimi.ingsw.model.card.CharacterCard;
 import it.polimi.ingsw.model.card.NoColour;
 import it.polimi.ingsw.model.card.ProtectIsland;
+import it.polimi.ingsw.model.player.Wizard;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,14 +22,11 @@ class ExpertGameTest {
 
     @Test
     void addCharacterCard() {
-
         assertEquals(3, gameController.getGame().getNumberOfCharacterCard());
-
     }
 
     @Test
     void hasProtectedIslandCard() {
-
         if (gameController.getGame().getCharacterCardsByIndex(0) instanceof ProtectIsland ||
                 gameController.getGame().getCharacterCardsByIndex(1) instanceof ProtectIsland ||
                 gameController.getGame().getCharacterCardsByIndex(2) instanceof ProtectIsland) {
@@ -39,6 +38,13 @@ class ExpertGameTest {
 
     @Test
     void getCoins() {
+        assertEquals(20, gameController.getGame().getCoins());
+        gameController.addPlayer("Viola", Wizard.TYPE_1);
+        assertEquals(19, gameController.getGame().getCoins());
+        gameController.addPlayer("Laura", Wizard.TYPE_2);
+        assertEquals(18, gameController.getGame().getCoins());
+        gameController.addPlayer("Sara", Wizard.TYPE_3);
+        assertEquals(17, gameController.getGame().getCoins());
         gameController.getGame().setCoins(20);
         assertEquals(20, gameController.getGame().getCoins());
     }
@@ -57,5 +63,31 @@ class ExpertGameTest {
         gameController.getGame().setActiveCharacterCard(card1);
         assertEquals(card1, gameController.getGame().getActiveCharacterCard());
 
+    }
+
+    @RepeatedTest(10)
+    void setting(){
+        gameController.addPlayer("Viola", Wizard.TYPE_1);
+        gameController.addPlayer("Laura", Wizard.TYPE_2);
+        gameController.addPlayer("Sara", Wizard.TYPE_3);
+        gameController.playAssistantCard(0, 0);
+        gameController.playAssistantCard(1, 1);
+        gameController.playAssistantCard(2, 2);
+        if(gameController.getGame().getCharacterCardsByIndex(0).getCost() == 1){
+            gameController.playCharacterCard(0, 0);
+            assertTrue(gameController.getGame().getHasPlayedCharacterCard());
+            assertEquals(0, gameController.getGame().getPlayerByIndex(0).getCoins());
+            assertEquals(2, gameController.getGame().getActiveCharacterCard().getCost());
+        } else if(gameController.getGame().getCharacterCardsByIndex(1).getCost() == 1){
+            gameController.playCharacterCard(0, 1);
+            assertTrue(gameController.getGame().getHasPlayedCharacterCard());
+            assertEquals(0, gameController.getGame().getPlayerByIndex(0).getCoins());
+            assertEquals(2, gameController.getGame().getActiveCharacterCard().getCost());
+        } else if(gameController.getGame().getCharacterCardsByIndex(2).getCost() == 1){
+            gameController.playCharacterCard(0, 2);
+            assertTrue(gameController.getGame().getHasPlayedCharacterCard());
+            assertEquals(0, gameController.getGame().getPlayerByIndex(0).getCoins());
+            assertEquals(2, gameController.getGame().getActiveCharacterCard().getCost());
+        }
     }
 }
