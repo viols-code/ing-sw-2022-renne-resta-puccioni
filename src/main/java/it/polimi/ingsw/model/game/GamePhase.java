8306a2acdc3c4333f70fb086d1479ceed5b70d34
viols-code <viola.renne@gmail.com>
2022-03-27 12:05:35 +0200

    Modified GamePhase and TurnPhase

--- src/main/java/it/polimi/ingsw/model/game/GamePhase.java
+++ src/main/java/it/polimi/ingsw/model/game/GamePhase.java
@@ -6,10 +6,9 @@ package it.polimi.ingsw.model.game;
  * @version 1.0
  */
 public enum GamePhase {
-    PLAYING,
-    PLAY_ASSISTANT_CARD,
     SETTING,
-    SET_CLOUD_TILE,
+    PLAY_ASSISTANT_CARD,
+    PLAYING,
     END_GAME
 }
 
