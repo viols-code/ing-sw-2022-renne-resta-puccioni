ead65c28119604f1ec54c3084eafe5f7b188171f
sararesta <sara.resta00@gmail.com>
2022-04-28 09:49:26 +0200

    Added view message MoveMotherNature

---
+++ src/main/java/it/polimi/ingsw/view/messages/MoveMotherNature.java
@@ -0,0 +1,19 @@
+package it.polimi.ingsw.view.messages;
+
+import it.polimi.ingsw.controller.GameController;
+
+import java.io.Serial;
+
+public class MoveMotherNature extends PlayerAction{
+    @Serial
+    private static final long serialVersionUID = -695696550449539585L;
+    private int player;
+    private final int movement;
+    public MoveMotherNature(int player, int movement){
+        this.player = player;
+        this.movement = movement;
+    }
+    public void process(GameController gameController){
+        gameController.moveMotherNature(player,movement);
+    }
+}
