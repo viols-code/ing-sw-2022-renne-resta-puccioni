7f46632394e5843447c06f5dfac4500372ac4bb8
sararesta <sara.resta00@gmail.com>
2022-04-29 18:49:52 +0200

    Updated CharacterCardEnumeration

--- src/main/java/it/polimi/ingsw/view/beans/CharacterCardEnumeration.java
+++ src/main/java/it/polimi/ingsw/view/beans/CharacterCardEnumeration.java
@@ -1,4 +1,16 @@
 package it.polimi.ingsw.view.beans;
 
 public enum CharacterCardEnumeration {
+    EXCHANGE_ENTRANCE_DINING_ROOM,
+    STUDENT_TO_ENTRANCE,
+    STUDENT_TO_DINING_ROOM,
+    PROTECT_ISLAND,
+    THREE_STUDENT,
+    ISLAND_INFLUENCE,
+    MOTHER_NATURE_MOVEMENT,
+    NO_COLOUR,
+    NO_TOWER,
+    STUDENT_TO_ISLAND,
+    TAKE_PROFESSOR,
+    TWO_POINTS
 }
