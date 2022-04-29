eb36a6f89234c9bb772ec1c2ca3ebfd6651887b4
sararesta <sara.resta00@gmail.com>
2022-04-29 20:19:29 +0200

    Updated MockSingleIsland: added attributes, getters, setters and javadoc

--- src/main/java/it/polimi/ingsw/view/beans/MockSingleIsland.java
+++ src/main/java/it/polimi/ingsw/view/beans/MockSingleIsland.java
@@ -1,4 +1,41 @@
 package it.polimi.ingsw.view.beans;
 
+import it.polimi.ingsw.model.Colour;
+
+import java.util.HashMap;
+
+/**
+ * Class which represents a local copy of the single islands
+ */
 public class MockSingleIsland {
+    /**
+     * An HashMap containing the number of students for each colour on the single islands
+     */
+    private final HashMap<Colour,Integer> students;
+
+    /**
+     * Constructs the single island
+     */
+    public MockSingleIsland() {
+        students = new HashMap<>();
+    }
+
+    /**
+     * Adds a students to the single island
+     *
+     * @param colour the colour of the student to add
+     */
+    public void setStudent(Colour colour){
+        students.replace(colour,students.get(colour),students.get(colour) + 1);
+    }
+
+    /**
+     * Gest the number of the students of the selected colour
+     *
+     * @param colour the colour
+     * @return the number of students of the colour selected
+     */
+    public int getStudents(Colour colour){
+        return students.get(colour);
+    }
 }
