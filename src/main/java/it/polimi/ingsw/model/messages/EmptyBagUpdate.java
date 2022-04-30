2a96271614b07bbadedd8ad9506df96abea0d071
Laura Puccioni <laura.puccioni@mail.polimi.it>
2022-04-30 17:23:22 +0200

    Created EmptyBagUpdate

---
+++ src/main/java/it/polimi/ingsw/model/messages/EmptyBagUpdate.java
@@ -0,0 +1,16 @@
+package it.polimi.ingsw.model.messages;
+
+import it.polimi.ingsw.view.View;
+
+import java.io.Serial;
+
+public class EmptyBagUpdate extends TableUpdate{
+
+    @Serial
+    private static final long serialVersionUID = 7611475659820901831L;
+
+    @Override
+    public void process(View view){
+
+    }
+}
