d90ab416672f6b59ceb4fb95aaf545192f499014
Laura Puccioni <laura.puccioni@mail.polimi.it>
2022-04-30 17:05:20 +0200

    TableUpdate

---
+++ src/main/java/it/polimi/ingsw/model/messages/TableUpdate.java
@@ -0,0 +1,9 @@
+package it.polimi.ingsw.model.messages;
+
+import java.io.Serial;
+
+public abstract class TableUpdate extends ModelUpdate{
+
+    @Serial
+    private static final long serialVersionUID = 7611410959820901831L;
+}
