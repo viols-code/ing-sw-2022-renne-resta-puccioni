4419e8a795dceec511f7948e3a186191463a5b31
Laura Puccioni <laura.puccioni@mail.polimi.it>
2022-04-30 17:27:00 +0200

    Created GroupIsland updates

---
+++ src/main/java/it/polimi/ingsw/model/messages/InfluencePlayerUpdate.java
@@ -0,0 +1,15 @@
+package it.polimi.ingsw.model.messages;
+
+import it.polimi.ingsw.view.View;
+
+import java.io.Serial;
+
+public class InfluencePlayerUpdate extends GroupIslandUpdate {
+    @Serial
+    private static final long serialVersionUID = 8611475659820921831L;
+
+    @Override
+    public void process(View view){
+
+    }
+}
---
+++ src/main/java/it/polimi/ingsw/model/messages/NoEntryTileUpdate.java
@@ -0,0 +1,15 @@
+package it.polimi.ingsw.model.messages;
+
+import it.polimi.ingsw.view.View;
+
+import java.io.Serial;
+
+public class NoEntryTileUpdate extends GroupIslandUpdate{
+    @Serial
+    private static final long serialVersionUID = 8811455658820901831L;
+
+    @Override
+    public void process(View view){
+
+    }
+}
