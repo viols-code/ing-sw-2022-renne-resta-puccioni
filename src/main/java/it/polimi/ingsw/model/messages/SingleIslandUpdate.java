d66e8ff7965c1b7e114f2031502e336203f3218c
Laura Puccioni <laura.puccioni@mail.polimi.it>
2022-04-30 17:25:05 +0200

    Created SingleIslandUpdate

---
+++ src/main/java/it/polimi/ingsw/model/messages/SingleIslandUpdate.java
@@ -0,0 +1,15 @@
+package it.polimi.ingsw.model.messages;
+
+import it.polimi.ingsw.view.View;
+
+import java.io.Serial;
+
+public class SingleIslandUpdate extends TableUpdate{
+    @Serial
+    private static final long serialVersionUID = 7611475669820901831L;
+
+    @Override
+    public void process(View view){
+
+    }
+}
