81df2eee2acd24866a8daaae06a6bc510045a73f
Laura Puccioni <laura.puccioni@mail.polimi.it>
2022-04-30 17:24:07 +0200

    Created CloudTileUpdate

---
+++ src/main/java/it/polimi/ingsw/model/messages/CloudTileUpdate.java
@@ -0,0 +1,15 @@
+package it.polimi.ingsw.model.messages;
+
+import it.polimi.ingsw.view.View;
+
+import java.io.Serial;
+
+public class CloudTileUpdate extends TableUpdate{
+    @Serial
+    private static final long serialVersionUID = 7611479959820901831L;
+
+    @Override
+    public void process(View view){
+
+    }
+}
