24f82c849c74c91663ce8a768d0d035734e11d57
Laura Puccioni <laura.puccioni@mail.polimi.it>
2022-05-01 10:18:58 +0200

    MotherNaturePositionUpdate and SingleIslandUpdate

--- src/main/java/it/polimi/ingsw/model/messages/MotherNaturePositionUpdate.java
+++ src/main/java/it/polimi/ingsw/model/messages/MotherNaturePositionUpdate.java
@@ -8,6 +8,11 @@ public class MotherNaturePositionUpdate extends TableUpdate{
 
     @Serial
     private static final long serialVersionUID = 7611410559820901831L;
+    private int movement;
+
+    public MotherNaturePositionUpdate(int movement){
+        this.movement = movement;
+    }
 
     @Override
     public void process(View view){
--- src/main/java/it/polimi/ingsw/model/messages/SingleIslandUpdate.java
+++ src/main/java/it/polimi/ingsw/model/messages/SingleIslandUpdate.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.model.messages;
 
+import it.polimi.ingsw.model.Colour;
 import it.polimi.ingsw.view.View;
 
 import java.io.Serial;
@@ -7,6 +8,11 @@ import java.io.Serial;
 public class SingleIslandUpdate extends TableUpdate{
     @Serial
     private static final long serialVersionUID = 7611475669820901831L;
+    private Colour colour;
+
+    public SingleIslandUpdate(Colour colour){
+        this.colour = colour;
+    }
 
     @Override
     public void process(View view){
