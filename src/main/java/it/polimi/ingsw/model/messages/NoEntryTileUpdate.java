4be019119f738ef87f6d517a40bdbb6ea7c50b84
Laura Puccioni <laura.puccioni@mail.polimi.it>
2022-05-01 10:22:35 +0200

    NoEntryTileUpdate

--- src/main/java/it/polimi/ingsw/model/messages/NoEntryTileUpdate.java
+++ src/main/java/it/polimi/ingsw/model/messages/NoEntryTileUpdate.java
@@ -7,6 +7,11 @@ import java.io.Serial;
 public class NoEntryTileUpdate extends GroupIslandUpdate{
     @Serial
     private static final long serialVersionUID = 8811455658820901831L;
+    private int numberOfNoEntryTiles;
+
+    public NoEntryTileUpdate(int numberOfNoEntryTiles){
+        this.numberOfNoEntryTiles = numberOfNoEntryTiles;
+    }
 
     @Override
     public void process(View view){
