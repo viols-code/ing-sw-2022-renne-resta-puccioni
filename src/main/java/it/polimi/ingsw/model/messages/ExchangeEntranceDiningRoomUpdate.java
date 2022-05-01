5f0434b2716da4a209a419090fa500ffdb7e810f
Laura Puccioni <laura.puccioni@mail.polimi.it>
2022-05-01 10:14:11 +0200

    ExchangeEntranceDiningRoomUpdate

--- src/main/java/it/polimi/ingsw/model/messages/ExchangeEntranceDiningRoomUpdate.java
+++ src/main/java/it/polimi/ingsw/model/messages/ExchangeEntranceDiningRoomUpdate.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.model.messages;
 
+import it.polimi.ingsw.model.Colour;
 import it.polimi.ingsw.view.View;
 
 import java.io.Serial;
@@ -7,6 +8,13 @@ import java.io.Serial;
 public class ExchangeEntranceDiningRoomUpdate extends CardUpdate{
     @Serial
     private static final long serialVersionUID = 5511475659820901831L;
+    private Colour colourEntrance;
+    private Colour colourDiningRoom;
+
+    public ExchangeEntranceDiningRoomUpdate(Colour colourEntrance, Colour colourDiningRoom){
+        this.colourDiningRoom = colourDiningRoom;
+        this.colourEntrance = colourEntrance;
+    }
 
     @Override
     public void process(View view){
