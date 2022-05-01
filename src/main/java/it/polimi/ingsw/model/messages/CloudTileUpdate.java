65729b8ce0f30b43415cdf468a6f9a0d5e374cdd
Laura Puccioni <laura.puccioni@mail.polimi.it>
2022-05-01 10:09:37 +0200

    CloudTileUpdate

--- src/main/java/it/polimi/ingsw/model/messages/CloudTileUpdate.java
+++ src/main/java/it/polimi/ingsw/model/messages/CloudTileUpdate.java
@@ -1,12 +1,19 @@
 package it.polimi.ingsw.model.messages;
 
+import it.polimi.ingsw.model.Colour;
 import it.polimi.ingsw.view.View;
 
 import java.io.Serial;
+import java.util.HashMap;
 
 public class CloudTileUpdate extends TableUpdate{
     @Serial
     private static final long serialVersionUID = 7611479959820901831L;
+    private HashMap<Colour, Integer> students;
+
+    public CloudTileUpdate(HashMap<Colour, Integer> students){
+        this.students = students;
+    }
 
     @Override
     public void process(View view){
