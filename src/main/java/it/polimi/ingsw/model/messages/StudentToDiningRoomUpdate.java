e4920c8450b4e7277be196e1c27bea93bcf75008
Laura Puccioni <laura.puccioni@mail.polimi.it>
2022-05-01 10:15:10 +0200

    StudentToDiningRoomUpdate

--- src/main/java/it/polimi/ingsw/model/messages/StudentToDiningRoomUpdate.java
+++ src/main/java/it/polimi/ingsw/model/messages/StudentToDiningRoomUpdate.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.model.messages;
 
+import it.polimi.ingsw.model.Colour;
 import it.polimi.ingsw.view.View;
 
 import java.io.Serial;
@@ -7,6 +8,12 @@ import java.io.Serial;
 public class StudentToDiningRoomUpdate extends CardUpdate{
     @Serial
     private static final long serialVersionUID = -7699975659820901831L;
+
+    private Colour colour;
+
+    public StudentToDiningRoomUpdate(Colour colour){
+        this.colour = colour;
+    }
 
     @Override
     public void process(View view){
