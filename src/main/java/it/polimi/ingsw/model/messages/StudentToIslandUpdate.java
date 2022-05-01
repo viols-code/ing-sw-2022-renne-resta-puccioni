a302bb1a314a5dede379dc596e2522f83b928de1
Laura Puccioni <laura.puccioni@mail.polimi.it>
2022-05-01 10:15:55 +0200

    StudentToIslandUpdate

--- src/main/java/it/polimi/ingsw/model/messages/StudentToIslandUpdate.java
+++ src/main/java/it/polimi/ingsw/model/messages/StudentToIslandUpdate.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.model.messages;
 
+import it.polimi.ingsw.model.Colour;
 import it.polimi.ingsw.view.View;
 
 import java.io.Serial;
@@ -7,6 +8,11 @@ import java.io.Serial;
 public class StudentToIslandUpdate extends CardUpdate{
     @Serial
     private static final long serialVersionUID = 8311475659746901831L;
+    private Colour colour;
+
+    public StudentToIslandUpdate(Colour colour){
+        this.colour = colour;
+    }
 
     @Override
     public void process(View view){
