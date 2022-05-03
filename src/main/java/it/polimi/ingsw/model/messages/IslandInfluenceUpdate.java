1bf39dc8db9a3d89cc7acbf65c2a04bb52692a3d
viols-code <viola.renne@gmail.com>
2022-05-03 14:16:01 +0200

    Created IslandInfluenceUpdate

---
+++ src/main/java/it/polimi/ingsw/model/messages/IslandInfluenceUpdate.java
@@ -0,0 +1,20 @@
+package it.polimi.ingsw.model.messages;
+
+import it.polimi.ingsw.view.View;
+
+import java.io.Serial;
+
+public class IslandInfluenceUpdate extends CardUpdate{
+    @Serial
+    private static final long serialVersionUID = -1023219829671352198L;
+    private final int groupIsland;
+
+    public IslandInfluenceUpdate(int groupIsland){
+        this.groupIsland = groupIsland;
+    }
+
+    @Override
+    public void process(View view) {
+
+    }
+}
