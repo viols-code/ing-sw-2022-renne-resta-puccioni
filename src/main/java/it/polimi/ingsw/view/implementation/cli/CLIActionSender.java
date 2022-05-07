02c3d50ee050f6a27f0d88582e81e8444c6d01f0
viols-code <viola.renne@gmail.com>
2022-05-07 11:41:58 +0200

    Modified CLI

--- src/main/java/it/polimi/ingsw/view/implementation/cli/CLIActionSender.java
+++ src/main/java/it/polimi/ingsw/view/implementation/cli/CLIActionSender.java
@@ -3,8 +3,5 @@ package it.polimi.ingsw.view.implementation.cli;
 import it.polimi.ingsw.view.ActionSender;
 import it.polimi.ingsw.view.View;
 
-public class CLIActionSender {
-    public CLIActionSender(View view) {
-        super(view);
-    }
+public class CLIActionSender{
 }
