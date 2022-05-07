626c581959770c2f1b6e443be7c3a022d6409892
viols-code <viola.renne@gmail.com>
2022-05-07 17:32:30 +0200

    Start implementing CLI.java

--- src/main/java/it/polimi/ingsw/view/implementation/cli/CLI.java
+++ src/main/java/it/polimi/ingsw/view/implementation/cli/CLI.java
@@ -1,6 +1,37 @@
 package it.polimi.ingsw.view.implementation.cli;
 
+import it.polimi.ingsw.client.Client;
 import it.polimi.ingsw.view.View;
 
-public class CLI {
+import java.util.Map;
+
+public class CLI extends View{
+    /**
+     * Constructs a new View.
+     *
+     * @param client the client responsible for this view
+     */
+    public CLI(Client client) {
+        super(client);
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    @Override
+    public void handlePlayerDisconnect(String playerName) {
+
+    }
+
+    @Override
+    public void handlePlayerCrash(String playerName) {
+
+    }
+
+    @Override
+    public void handleEndGame(Map<String, Integer> scores, String winnerName) {
+
+    }
 }
