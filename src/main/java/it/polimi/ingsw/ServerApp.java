9d9b7ac97e7f693dacb61598678987c24f9b747e
viols-code <viola.renne@gmail.com>
2022-05-08 18:09:38 +0200

    Modified ServerApp.java in order to do a test

--- src/main/java/it/polimi/ingsw/ServerApp.java
+++ src/main/java/it/polimi/ingsw/ServerApp.java
@@ -3,6 +3,7 @@ package it.polimi.ingsw;
 import it.polimi.ingsw.server.Server;
 
 import java.io.IOException;
+import java.util.Locale;
 
 /**
  * Launcher class for the game server.
@@ -15,6 +16,19 @@ public class ServerApp {
      */
     public static void main(String[] args) {
         Server server;
+        String command = "view ciao bella";
+
+        String[] split = command.split(" ");
+        String cmd = split[0];
+        String[] ciao = new String[0];
+        if (cmd.equals("view")) {
+            split[1] = split[1].substring(0, 1).toUpperCase(Locale.ROOT) + split[1].substring(1);
+            for (int i = 2; i < split.length; i++) {
+                split[1] += split[i].substring(0, 1).toUpperCase(Locale.ROOT) + split[i].substring(1);
+            }
+        }
+
+        System.out.println(split[1]);
         try {
             server = new Server();
             server.run();
