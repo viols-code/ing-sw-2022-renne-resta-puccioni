429aa72a2511a2ab9684d812a6577cec2077e445
sararesta <sara.resta00@gmail.com>
2022-05-09 08:28:57 +0200

    Added attributes to SingleIsland.addStudent to fix problems due to notify

--- src/main/java/it/polimi/ingsw/model/messages/SingleIslandUpdate.java
+++ src/main/java/it/polimi/ingsw/model/messages/SingleIslandUpdate.java
@@ -8,9 +8,13 @@ import java.io.Serial;
 public class SingleIslandUpdate extends TableUpdate {
     @Serial
     private static final long serialVersionUID = 7611475669820901831L;
-    private Colour colour;
+    private final int groupIsland;
+    private final int singleIsland;
+    private final Colour colour;
 
-    public SingleIslandUpdate(Colour colour) {
+    public SingleIslandUpdate(int groupIsland, int singleIsland, Colour colour) {
+        this.groupIsland = groupIsland;
+        this.singleIsland = singleIsland;
         this.colour = colour;
     }
 
--- src/main/java/it/polimi/ingsw/model/table/island/SingleIsland.java
+++ src/main/java/it/polimi/ingsw/model/table/island/SingleIsland.java
@@ -48,10 +48,12 @@ public class SingleIsland extends Observable<IServerPacket> {
      * Adds a student of the specified colour to the SingleIsland
      *
      * @param colour the colour of the student to be added
+     * @param groupIslandIndex the index of the group island -> useful to notify the change to the client
+     * @param singleIslandIndex the index of the single island -> useful to notify the change to the client
      */
-    public void addStudent(Colour colour) {
+    public void addStudent(int groupIslandIndex, int singleIslandIndex, Colour colour) {
         students.replace(colour, students.get(colour), students.get(colour) + 1);
-        notify(new SingleIslandUpdate(colour));
+        notify(new SingleIslandUpdate(groupIslandIndex,singleIslandIndex, colour));
     }
 
 
