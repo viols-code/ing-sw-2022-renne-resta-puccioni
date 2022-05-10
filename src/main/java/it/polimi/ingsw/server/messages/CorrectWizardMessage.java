e3fbde6fcd45b9bb6ec539f68d501c7d01f2c0a9
viols-code <viola.renne@gmail.com>
2022-05-10 10:57:27 +0200

    Created CorrectWizardMessage.java

---
+++ src/main/java/it/polimi/ingsw/server/messages/CorrectWizardMessage.java
@@ -0,0 +1,33 @@
+package it.polimi.ingsw.server.messages;
+
+import it.polimi.ingsw.model.player.Wizard;
+import it.polimi.ingsw.server.SocketClientConnection;
+import it.polimi.ingsw.view.View;
+
+import java.io.Serial;
+import java.util.List;
+
+public class CorrectWizardMessage extends DirectServerMessage{
+    @Serial
+    private static final long serialVersionUID = -7978594154814429984L;
+
+    private final Wizard wizard;
+    private final List<Wizard> takenWizard;
+
+    /**
+     * Constructs a new PlayerConnectMessage for the player with the given name.
+     *
+     * @param wizard     the wizard chosen by the player
+     * @param takenWizard the list of wizards already taken
+     */
+    public CorrectWizardMessage(SocketClientConnection recipient, Wizard wizard, List<Wizard> takenWizard) {
+        super(recipient);
+        this.wizard = wizard;
+        this.takenWizard = takenWizard;
+    }
+
+    @Override
+    public void process(View view) {
+        view.handleCorrentWizard(wizard, takenWizard);
+    }
+}
