3a3f5fdb9cce0e4e33a4ed77a98063fb8e1c7156
viols-code <viola.renne@gmail.com>
2022-05-10 13:50:20 +0200

    Create a new model message: CurrentPlayerUpdate.java

---
+++ src/main/java/it/polimi/ingsw/model/messages/CurrentPlayerUpdate.java
@@ -0,0 +1,21 @@
+package it.polimi.ingsw.model.messages;
+
+import it.polimi.ingsw.model.game.TurnPhase;
+import it.polimi.ingsw.view.View;
+
+import java.io.Serial;
+
+public class CurrentPlayerUpdate extends GameUpdate{
+    @Serial
+    private static final long serialVersionUID = 1037513789242382023L;
+    private final String currentPlayer;
+
+    public CurrentPlayerUpdate(String currentPlayer) {
+        this.currentPlayer = currentPlayer;
+    }
+
+    @Override
+    public void process(View view) {
+        view.getModelUpdateHandler().updateCurrentPlayer(currentPlayer);
+    }
+}
