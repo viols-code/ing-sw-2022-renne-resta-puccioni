02be9388d5ce4b645123f8901d269e383f4021ec
viols-code <viola.renne@gmail.com>
2022-05-10 12:33:35 +0200

    Modified PlayerConnectMessage.java

--- src/main/java/it/polimi/ingsw/server/messages/PlayerConnectMessage.java
+++ src/main/java/it/polimi/ingsw/server/messages/PlayerConnectMessage.java
@@ -4,7 +4,6 @@ import it.polimi.ingsw.model.player.Wizard;
 import it.polimi.ingsw.view.View;
 
 import java.io.Serial;
-import java.util.List;
 
 /**
  * ServerMessage notifying all clients of a new client connection.
@@ -15,11 +14,8 @@ public class PlayerConnectMessage extends ServerMessage {
 
     private final String playerName;
     private final Wizard wizard;
-    private final boolean gameMode;
     private final int currentPlayers;
-    private final int playersToStart;
-    private List<String> otherConnectedPlayers;
-    private List<Wizard> takenWizards;
+    private final Integer playersToStart;
 
     /**
      * Constructs a new PlayerConnectMessage for the player with the given name.
@@ -28,17 +24,15 @@ public class PlayerConnectMessage extends ServerMessage {
      * @param currentPlayers the current number of players in the lobby
      * @param playersToStart the number of players required to start the game in the lobby
      */
-    // cosa fa
-    public PlayerConnectMessage(String playerName, Wizard wizard, boolean gameMode, int currentPlayers, int playersToStart) {
+    public PlayerConnectMessage(String playerName, Wizard wizard, int currentPlayers, Integer playersToStart) {
         this.playerName = playerName;
         this.wizard = wizard;
-        this.gameMode = gameMode;
         this.currentPlayers = currentPlayers;
         this.playersToStart = playersToStart;
     }
 
     @Override
     public void process(View view) {
-        view.handlePlayerConnect(playerName, wizard, gameMode, currentPlayers, playersToStart, otherConnectedPlayers);
+        view.handlePlayerConnect(playerName, wizard, currentPlayers, playersToStart);
     }
 }
