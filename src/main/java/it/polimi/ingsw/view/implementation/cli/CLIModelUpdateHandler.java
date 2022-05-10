8e3ccc54eb6c3a8c630c1e39212c210818d022f2
viols-code <viola.renne@gmail.com>
2022-05-10 15:10:56 +0200

    Started implementing CLIModelUpdateHandler.java

--- src/main/java/it/polimi/ingsw/view/implementation/cli/CLIModelUpdateHandler.java
+++ src/main/java/it/polimi/ingsw/view/implementation/cli/CLIModelUpdateHandler.java
@@ -1,4 +1,15 @@
 package it.polimi.ingsw.view.implementation.cli;
 
-public class CLIModelUpdateHandler {
+import it.polimi.ingsw.view.ModelUpdateHandler;
+import it.polimi.ingsw.view.View;
+
+public class CLIModelUpdateHandler extends ModelUpdateHandler {
+    /**
+     * Constructs a new ModelUpdateHandler.
+     *
+     * @param view the view responsible for this model update handler
+     */
+    protected CLIModelUpdateHandler(View view) {
+        super(view);
+    }
 }
