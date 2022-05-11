5373f92e9be9116e05ccbeea77abc5d0d23f03d2
Laura Puccioni <laura.puccioni@mail.polimi.it>
2022-05-11 16:49:21 +0200

    removed "\n"

--- src/main/java/it/polimi/ingsw/view/implementation/cli/CLIRenderer.java
+++ src/main/java/it/polimi/ingsw/view/implementation/cli/CLIRenderer.java
@@ -98,8 +98,6 @@ public class CLIRenderer extends Renderer {
                 cloudTile = cloudTile.concat("\n\t" +
                         AnsiColour.getStudentColour(colour) + colour.name() +": " + students.get(colour) + AnsiColour.RESET);
             }
-
-            System.out.println("\n");
         }
 
         System.out.println(cloudTile);
