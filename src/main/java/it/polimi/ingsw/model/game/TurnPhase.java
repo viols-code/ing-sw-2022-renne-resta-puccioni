8082c0da14af439df5862cc658be4f84d9c2e69a
viols-code <viola.renne@gmail.com>
2022-05-11 19:05:21 +0200

    Added TurnPhase in TurnPhase.java

--- src/main/java/it/polimi/ingsw/model/game/TurnPhase.java
+++ src/main/java/it/polimi/ingsw/model/game/TurnPhase.java
@@ -1,8 +1,10 @@
 package it.polimi.ingsw.model.game;
 
 public enum TurnPhase {
+    WAITING,
+    PLAY_ASSISTANT_CARD,
     MOVE_STUDENT,
     MOVE_MOTHER_NATURE,
     CHOOSE_CLOUD_TILE,
-    WAITING
+    ENDGAME
 }
