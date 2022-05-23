5bf822e2f6f35557887f104c336e7a90dc2cc4d9
Laura Puccioni <laura.puccioni@mail.polimi.it>
2022-05-23 17:49:19 +0200

    implemented NicknameSelection

--- src/main/java/it/polimi/ingsw/view/implementation/gui/controllers/NicknameSelection.java
+++ src/main/java/it/polimi/ingsw/view/implementation/gui/controllers/NicknameSelection.java
@@ -1,4 +1,22 @@
 package it.polimi.ingsw.view.implementation.gui.controllers;
 
+import it.polimi.ingsw.view.implementation.gui.GUI;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class NicknameSelection {
+
+    @FXML
+    private TextField username;
+    @FXML
+    private Button choose;
+
+    @FXML
+    private void initialize() {
+        choose.setOnAction(actionEvent -> {
+            if (!username.getText().isBlank())
+                GUI.instance().setPlayerName(username.getText());
+        });
+    }
 }
