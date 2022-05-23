c0fa10b0e6843495222fceb695aa61f6d710bb28
Laura Puccioni <laura.puccioni@mail.polimi.it>
2022-05-23 16:43:15 +0200

    modified

--- src/main/java/it/polimi/ingsw/view/implementation/gui/GUI.java
+++ src/main/java/it/polimi/ingsw/view/implementation/gui/GUI.java
@@ -2,6 +2,7 @@ package it.polimi.ingsw.view.implementation.gui;
 
 import it.polimi.ingsw.client.Client;
 import it.polimi.ingsw.view.View;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
@@ -10,6 +11,7 @@ import javafx.stage.Stage;
 public class GUI extends View {
     private final Stage stage;
     private Scene scene;
+    private static GUI instance;
     //private MediaPlayer mediaPlayer;
     private boolean wasPlayingMusicBefore;
 
@@ -20,11 +22,17 @@ public class GUI extends View {
         this.setModelUpdateHandler(new GUIModelUpdateHandler(this));
         this.setRenderer(new GUIRenderer(this));
         this.setActionSender(new GUIActionSender(this));
+        instance = this;
 
+    }
+
+    public static GUI instance() {
+        return instance;
     }
 
     @Override
     public void run() {
+        //Parent homePage = FXMLUtils.loadFXML("/gui/Home");
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
         Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
