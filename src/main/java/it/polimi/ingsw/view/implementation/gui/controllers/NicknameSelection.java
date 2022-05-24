ee1ede842f1327a37f65a5cb1593b31a6638d293
viols-code <viola.renne@gmail.com>
2022-05-24 14:14:49 +0200

    Nickname check in NicknameSelection

--- src/main/java/it/polimi/ingsw/view/implementation/gui/controllers/NicknameSelection.java
+++ src/main/java/it/polimi/ingsw/view/implementation/gui/controllers/NicknameSelection.java
@@ -3,6 +3,7 @@ package it.polimi.ingsw.view.implementation.gui.controllers;
 import it.polimi.ingsw.view.implementation.gui.GUI;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class NicknameSelection {
@@ -11,12 +12,26 @@ public class NicknameSelection {
     private TextField username;
     @FXML
     private Button choose;
+    @FXML
+    private Label wrongNickname;
 
     @FXML
     private void initialize() {
         choose.setOnAction(actionEvent -> {
-            if (!username.getText().isBlank())
-                GUI.instance().setPlayerName(username.getText());
+            if(username.getText().trim().length() != username.getText().length()){
+                wrongNickname.setText("The nickname must be without empty spaces");
+                wrongNickname.setVisible(true);
+            } else if(username.getText().split(" ").length > 1){
+                wrongNickname.setText("The nickname must be without empty spaces");
+                wrongNickname.setVisible(true);
+            } else{
+                if (!username.getText().isBlank())
+                    GUI.instance().setPlayerName(username.getText());
+                else{
+                    wrongNickname.setText("The nickname cannot be empty");
+                    wrongNickname.setVisible(true);
+                }
+            }
         });
     }
 }
