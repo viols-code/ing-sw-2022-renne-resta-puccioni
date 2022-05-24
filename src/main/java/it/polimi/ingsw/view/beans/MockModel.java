001d81a23a88c991e06122f420481163df7a0ebb
sararesta <sara.resta00@gmail.com>
2022-05-24 21:13:34 +0200

    Added javadoc in MockModel

--- src/main/java/it/polimi/ingsw/view/beans/MockModel.java
+++ src/main/java/it/polimi/ingsw/view/beans/MockModel.java
@@ -24,6 +24,9 @@ public class MockModel {
      */
     private final HashMap<String, MockPlayer> players;
 
+    /**
+     * The current player
+     */
     private MockPlayer currentPlayer;
 
     /**
