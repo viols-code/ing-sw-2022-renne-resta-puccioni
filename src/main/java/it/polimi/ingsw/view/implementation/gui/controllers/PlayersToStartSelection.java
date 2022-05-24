778d33bde4154909858044cc1fc2c0cf57a9de39
Laura Puccioni <laura.puccioni@mail.polimi.it>
2022-05-24 14:58:39 +0200

    implemented PlayersToStartSelection

--- src/main/java/it/polimi/ingsw/view/implementation/gui/controllers/PlayersToStartSelection.java
+++ src/main/java/it/polimi/ingsw/view/implementation/gui/controllers/PlayersToStartSelection.java
@@ -1,4 +1,17 @@
 package it.polimi.ingsw.view.implementation.gui.controllers;
 
+import it.polimi.ingsw.view.implementation.gui.GUI;
+import javafx.fxml.FXML;
+
 public class PlayersToStartSelection {
+
+    @FXML
+    private void twoPlayersGame(){
+        GUI.instance().getActionSender().setPlayersToStart(2);
+    }
+
+    @FXML
+    private void threePlayersGame(){
+        GUI.instance().getActionSender().setPlayersToStart(3);
+    }
 }
