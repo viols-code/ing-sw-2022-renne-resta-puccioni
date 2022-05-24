0aa72d12f31571e757094880407a72735306b1bf
sararesta <sara.resta00@gmail.com>
2022-05-24 12:53:58 +0200

    Added MenuButton in WizardSelection

--- src/main/java/it/polimi/ingsw/view/implementation/gui/controllers/WizardSelection.java
+++ src/main/java/it/polimi/ingsw/view/implementation/gui/controllers/WizardSelection.java
@@ -7,23 +7,35 @@ import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class WizardSelection {
 
     @FXML
-    private ListView<Wizard> wizards;
+    private MenuButton wizards;
 
-    Wizard[] types = {Wizard.TYPE_1, Wizard.TYPE_2, Wizard.TYPE_3, Wizard.TYPE_4};
 
     @FXML
-    private void initialize() {
-       wizards.getItems().addAll(types);
+    private void type_1(){
+        GUI.instance().setWizard(Wizard.TYPE_1);
+    }
 
-       wizards.getSelectionModel().selectedItemProperty().addListener((observableValue, wizard, t1) ->
-               GUI.instance().setWizard(wizards.getSelectionModel().getSelectedItem()));
+    @FXML
+    private void type_2(){
+        GUI.instance().setWizard(Wizard.TYPE_2);
+    }
+
+    @FXML
+    private void type_3(){
+        GUI.instance().setWizard(Wizard.TYPE_3);
+    }
+
+    @FXML
+    private void type_4(){
+        GUI.instance().setWizard(Wizard.TYPE_4);
     }
 
 }
