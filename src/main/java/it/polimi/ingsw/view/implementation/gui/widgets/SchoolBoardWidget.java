2ec3aabebf8dd1baf1845524f21d0a25cec41eb9
Laura Puccioni <laura.puccioni@mail.polimi.it>
2022-05-26 21:53:43 +0200

    schoolBoard widget

--- src/main/java/it/polimi/ingsw/view/implementation/gui/widgets/SchoolBoardWidget.java
+++ src/main/java/it/polimi/ingsw/view/implementation/gui/widgets/SchoolBoardWidget.java
@@ -1,4 +1,21 @@
 package it.polimi.ingsw.view.implementation.gui.widgets;
 
+import it.polimi.ingsw.view.beans.MockPlayer;
+import it.polimi.ingsw.view.implementation.gui.GUI;
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+
 public class SchoolBoardWidget {
+
+    @FXML
+    private Image studentEntrance1;
+
+    private MockPlayer localPlayer;
+
+    private void initialize(){
+
+       localPlayer = GUI.instance().getModel().getLocalPlayer();
+
+
+    }
 }
