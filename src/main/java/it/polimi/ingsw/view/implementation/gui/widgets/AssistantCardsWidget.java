a1b52861c431e48ca708398ac70988b5fce86bab
Laura Puccioni <laura.puccioni@mail.polimi.it>
2022-05-28 16:24:58 +0200

    Added button back to school board

--- src/main/java/it/polimi/ingsw/view/implementation/gui/widgets/AssistantCardsWidget.java
+++ src/main/java/it/polimi/ingsw/view/implementation/gui/widgets/AssistantCardsWidget.java
@@ -1,11 +1,18 @@
 package it.polimi.ingsw.view.implementation.gui.widgets;
 
 import it.polimi.ingsw.FXMLUtils;
+import it.polimi.ingsw.view.implementation.gui.GUI;
+import javafx.fxml.FXML;
 import javafx.scene.layout.StackPane;
 
 public class AssistantCardsWidget extends StackPane {
 
     public AssistantCardsWidget() {
         FXMLUtils.loadWidgetFXML(this);
+    }
+
+    @FXML
+    public void showSchoolBoard(){
+        GUI.instance().showPlayerBoard();
     }
 }
