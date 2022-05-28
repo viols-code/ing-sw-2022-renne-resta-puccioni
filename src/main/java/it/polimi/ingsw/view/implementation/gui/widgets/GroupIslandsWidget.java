9f41f34621d1e7777e2a3bd226bcd70c104f9e5c
Laura Puccioni <laura.puccioni@mail.polimi.it>
2022-05-28 16:21:17 +0200

    Added button back to school board

--- src/main/java/it/polimi/ingsw/view/implementation/gui/widgets/GroupIslandsWidget.java
+++ src/main/java/it/polimi/ingsw/view/implementation/gui/widgets/GroupIslandsWidget.java
@@ -1,11 +1,18 @@
 package it.polimi.ingsw.view.implementation.gui.widgets;
 
 import it.polimi.ingsw.FXMLUtils;
+import it.polimi.ingsw.view.implementation.gui.GUI;
+import javafx.fxml.FXML;
 import javafx.scene.layout.StackPane;
 
 public class GroupIslandsWidget extends StackPane {
 
     public GroupIslandsWidget() {
         FXMLUtils.loadWidgetFXML(this);
+    }
+
+    @FXML
+    public void showSchoolBoard(){
+        GUI.instance().showPlayerBoard();
     }
 }
