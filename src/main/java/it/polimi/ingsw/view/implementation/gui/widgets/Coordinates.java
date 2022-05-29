737064d3b1a89e0c9580f2537071e112a6f26b0a
sararesta <sara.resta00@gmail.com>
2022-05-29 20:37:43 +0200

    Added Coordinates to manage javafx GridPanes

---
+++ src/main/java/it/polimi/ingsw/view/implementation/gui/widgets/Coordinates.java
@@ -0,0 +1,18 @@
+package it.polimi.ingsw.view.implementation.gui.widgets;
+
+public class Coordinates {
+    private final int row;
+    private final int column;
+    public Coordinates(int x, int y){
+        row = x;
+        column = y;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+}
