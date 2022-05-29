c8b2b85e8ca6675d1f3af52cf34c17cfe1148b1d
sararesta <sara.resta00@gmail.com>
2022-05-29 20:38:30 +0200

    Added StudentCoordinates

---
+++ src/main/java/it/polimi/ingsw/view/beans/StudentCoordinates.java
@@ -0,0 +1,27 @@
+package it.polimi.ingsw.view.beans;
+
+import it.polimi.ingsw.model.Colour;
+
+public class StudentCoordinates {
+    private final int row;
+    private final int column;
+    private final Colour studentColour;
+
+    public StudentCoordinates(int row, int column, Colour studentColour){
+        this.row = row;
+        this.column = column;
+        this.studentColour = studentColour;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public Colour getStudentColour() {
+        return studentColour;
+    }
+}
