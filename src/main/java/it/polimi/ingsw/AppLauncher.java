0b176d11e301b5e9ca4d4cfea749c4ce33def11d
viols-code <viola.renne@gmail.com>
2022-06-04 10:36:23 +0200

    UTF for Windows

--- src/main/java/it/polimi/ingsw/AppLauncher.java
+++ src/main/java/it/polimi/ingsw/AppLauncher.java
@@ -2,9 +2,20 @@ package it.polimi.ingsw;
 
 import java.io.IOException;
 
+
+
 public class AppLauncher {
-    public static void main(String[] args) throws IOException, InterruptedException {
-        //new ProcessBuilder("cmd", "/c", "chcp 65001").inheritIO().start().waitFor();
+    private final static String operatingSystem = System.getProperty("os.name");
+
+    public static void main(String[] args) {
+
+        if(operatingSystem.contains("Windows")){
+            try{
+                new ProcessBuilder("cmd", "/c", "chcp 65001").inheritIO().start().waitFor();
+            } catch(InterruptedException | IOException e){
+                System.err.println(e.getMessage());
+            }
+        }
         App.main(args);
     }
 }
