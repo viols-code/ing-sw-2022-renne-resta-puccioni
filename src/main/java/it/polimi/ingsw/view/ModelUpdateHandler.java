6538792c818c2e9a11598b237b86111bd7de72cd
sararesta <sara.resta00@gmail.com>
2022-06-04 14:38:13 +0200

    Updated ModelUpdateHandler.updateUnifyIsland, added call to method MockGroupIsland.clearInfluentPlayerProperty

--- src/main/java/it/polimi/ingsw/view/ModelUpdateHandler.java
+++ src/main/java/it/polimi/ingsw/view/ModelUpdateHandler.java
@@ -285,6 +285,9 @@ public abstract class ModelUpdateHandler {
      * @param groupIsland2 the index of the second gorup island
      */
     public void updateUnifyIsland(int groupIsland1, int groupIsland2) {
+        for(MockGroupIsland groupIsland: view.getModel().getTable().getGroupIslands()){
+            groupIsland.clearInfluentPlayerProperty();
+        }
         getView().getModel().getTable().unify(groupIsland1, groupIsland2);
     }
 
