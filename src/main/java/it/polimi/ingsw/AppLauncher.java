222d2baccd2706b50b6919dffcbdacd3987ea0ae
Laura Puccioni <laura.puccioni@mail.polimi.it>
2022-06-24 13:09:40 +0200

    Added javadoc to AppLauncher

--- src/main/java/it/polimi/ingsw/AppLauncher.java
+++ src/main/java/it/polimi/ingsw/AppLauncher.java
@@ -2,10 +2,17 @@ package it.polimi.ingsw;
 
 import java.io.IOException;
 
-
+/**
+ * This class is needed for jar packaging with maven shade because the launcher class for a JavaFX project should not extend Application
+ */
 public class AppLauncher {
     private final static String operatingSystem = System.getProperty("os.name");
 
+    /**
+     * Launches the game client.
+     *
+     * @param args the argument: "cli" launches the game in command line mode
+     */
     public static void main(String[] args) {
         if (operatingSystem.contains("Windows")) {
             try {
