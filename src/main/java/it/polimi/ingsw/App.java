09c5ba737190448f13563c0f6a0fb19bd42ba204
Laura Puccioni <laura.puccioni@mail.polimi.it>
2022-06-24 13:07:08 +0200

    Added javadoc to App

--- src/main/java/it/polimi/ingsw/App.java
+++ src/main/java/it/polimi/ingsw/App.java
@@ -4,9 +4,17 @@ import it.polimi.ingsw.client.Client;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+/**
+ * Class used to launch the game client.
+ */
 public class App extends Application {
     private static boolean startCli = false;
 
+    /**
+     * Launches the game client.
+     *
+     * @param args the argument: "cli" launches the game in command line mode
+     */
     public static void main(String[] args) {
         if (1 <= args.length) {
             handleCommand(args[0]);
@@ -16,12 +24,22 @@ public class App extends Application {
         launch();
     }
 
+    /**
+     * Parses and recognizes the given command line argument.
+     *
+     * @param command the command given
+     */
     private static void handleCommand(String command) {
         if (command.equalsIgnoreCase("cli")) {
             startCli = true;
         }
     }
 
+    /**
+     * Entry method for the JavaFX application.
+     *
+     * @param stage the main stage of the application
+     */
     @Override
     public void start(Stage stage) {
         Client client;
