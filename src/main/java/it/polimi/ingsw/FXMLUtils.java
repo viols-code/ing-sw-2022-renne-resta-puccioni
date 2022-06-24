64bb754fa1b42e8ab91a1c067f083336ea8e910c
Laura Puccioni <laura.puccioni@mail.polimi.it>
2022-06-24 13:13:52 +0200

    Added javadoc to FXMLUtils

--- src/main/java/it/polimi/ingsw/FXMLUtils.java
+++ src/main/java/it/polimi/ingsw/FXMLUtils.java
@@ -6,8 +6,17 @@ import javafx.scene.control.Label;
 
 import java.io.IOException;
 
+/**
+ * Class used for FXML loading.
+ */
 public class FXMLUtils {
 
+    /**
+     * Loads an FXML page
+     *
+     * @param fxml the path of the fxml file
+     * @return the loaded page
+     */
     public static Parent loadFXML(String fxml) {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
@@ -18,11 +27,24 @@ public class FXMLUtils {
         }
     }
 
+    /**
+     * Load a widget's FXML file
+     *
+     * @param component the controller for the widget
+     * @param <T> the type that is the controller for the widget
+     */
     public static <T extends Parent> void loadWidgetFXML(T component) {
         String fileName = "/widget/" + component.getClass().getSimpleName() + ".fxml";
         loadFXMLComponent(component, fileName);
     }
 
+    /**
+     * Load a widget's FXML.
+     *
+     * @param component the controller for the widget
+     * @param fileName the path to the fxml file
+     * @param <T> the type that is the controller for the widget
+     */
     public static <T extends Parent> void loadFXMLComponent(T component, String fileName) {
         FXMLLoader loader = new FXMLLoader(App.class.getResource(fileName));
         loader.setRoot(component);
